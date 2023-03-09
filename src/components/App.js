01d6da708419b8e66445a2d73d9188b7f51feb23
MITTAL KUMAR GUPTA <116200804+crushonpuchhu@users.noreply.github.com>
2023-03-09 19:53:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,8 @@ const App = () => {
 
   return (
     <div id="main">
-      <GoalForm />
-      <ListOfGoals />
+      <GoalForm setAllGoalsProps={setAllGoals} allGoalsProps={allGoals} />
+      <ListOfGoals allGoalsProps={allGoals} />
     </div>
   )
   
